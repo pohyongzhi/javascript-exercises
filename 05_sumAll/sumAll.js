845c718233a5e37edf10a5c8a4bad27187b66e4a
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-06-11 20:09:48 +0800

    Added sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (!(typeof start === "number") || !(typeof end === "number") || start < 0) {
+    return "ERROR";
+  }
 
+  let result = 0;
+
+  //   Swap start and end if start > end
+  if (start > end) {
+    let temp = start;
+    start = end;
+    end = temp;
+  }
+
+  for (let i = start; i <= end; i++) {
+    result += i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
