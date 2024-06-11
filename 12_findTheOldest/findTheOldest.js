478991ff25585d14a384fb0e62dd7125af6ca81f
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-06-12 01:01:34 +0800

    Added solution for 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
+const findAge = (person) => {
+    let yearOfBirth = person.yearOfBirth;
+    let yearOfDeath = person.yearOfDeath;
 
+    if (yearOfDeath === undefined) {
+        let today = new Date();
+
+        return today.getFullYear() - yearOfBirth;
+    }
+
+    return yearOfDeath - yearOfBirth;
+};
+
+const findTheOldest = function (people) {
+    let oldestAge = 0;
+    let oldestPerson = undefined;
+
+    // Find the oldest age
+    people.forEach((p) => {
+        let currAge = findAge(p);
+
+        if (currAge > oldestAge) {
+            oldestAge = currAge;
+            oldestPerson = p;
+        }
+    });
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
