a9832ea079f7e48b509b542d64a0d331e856b2fb
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-06-12 00:17:28 +0800

    Added 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+  let results = [];
 
+  books.forEach((book) => {
+    results.push(book["title"]);
+  });
+
+  return results;
 };
 
 // Do not edit below this line
