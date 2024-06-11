49f567635802005e5cb8764457fcbc40019461ad
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-06-12 00:13:39 +0800

    Added solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (typeof n === "string") {
+    n = parseInt(n);
+  }
 
+  if (n < 0) {
+    return "OOPS";
+  }
+
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  }
+
+  let num1 = 1;
+  let num2 = 1;
+
+  for (let i = 1; i < n; i++) {
+    let temp = num1 + num2;
+    num1 = num2;
+    num2 = temp;
+  }
+
+  console.log(num1);
+  console.log(num2);
+
+  return num1;
 };
 
 // Do not edit below this line
