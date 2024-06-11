9dac749d765dc3fcba048f8fe27704b7f0a0de4e
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-06-12 00:00:58 +0800

    Added solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let cleanedStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  let reversedStr = "";
 
+  reversedStr = cleanedStr.split("").reverse().join("");
+
+  console.log(cleanedStr);
+  console.log(reversedStr);
+
+  return cleanedStr === reversedStr;
 };
 
 // Do not edit below this line
